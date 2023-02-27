f7366f44c6bfb3a604c105ee5851b5a553abe887
devkjoon <devkjoon@gmail.com>
2023-02-27 12:22:34 -0500

    server.js setup

--- models/User.js
+++ models/User.js
@@ -1,45 +1,45 @@
 const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema(
-    {
-        username: {
-            type: String,
-            unique: true,
-            trim: true, 
-            required: "Username is required",
-        },
-
-        email: {
-            type: String,
-            unique: true,
-            required: "Username is required",
-            match: [/.+@.+\..+/],
-        },
-
-        thoughts: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "Thought",
-            },
-        ],
-
-        friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "User",
-            },
-        ],
+  {
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: "Username is required",
     },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        id: false,
-    }
+
+    email: {
+      type: String,
+      unique: true,
+      required: "Username is required",
+      match: [/.+@.+\..+/],
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
 UserSchema.virtual("friendCount").get(function () {
-    return this.friends.length;
+  return this.friends.length;
 });
 
 const User = model("User", UserSchema);
